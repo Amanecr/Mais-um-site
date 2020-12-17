4d88a5b2b2133403ded5e4ebd3771268ce857268
Amanecr <amanecribeiro123@gmail.com>
2020-12-17 16:59:31 -0300

    V 0.5

---
+++ script.js
@@ -0,0 +1,8 @@
+var hamburguer = document.querySelector(".hamburguer")
+
+hamburguer .addEventListener ("click", function() {
+   var sidebar = document.querySelector (".sidebar") .classList.toggle(".show-menu");
+});
+
+//O comando para abrir e fechar o menu a patir do js não funciona. O que estou fazendo de errado?
+// 34:15 #03
